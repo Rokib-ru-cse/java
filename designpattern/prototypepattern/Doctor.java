98719b45985e70bf5353cb07ff373d9cfb240e32
rokib_ru_cse <rokib.ahmed@bizzntek.com>
2023-07-03 21:24:02 +0600

    design pattern

---
+++ designpattern/prototypepattern/Doctor.java
@@ -0,0 +1,9 @@
+package designpattern.prototypepattern;
+
+
+public class Doctor extends Profession {
+    @Override
+    public void print() {
+        System.out.println("i am doctor");
+    }
+}
---
+++ designpattern/prototypepattern/Teacher.java
@@ -0,0 +1,10 @@
+package designpattern.prototypepattern;
+
+
+
+public class Teacher extends Profession {
+    @Override
+    public void print() {
+        System.out.println("i am teacher");
+    }
+}
