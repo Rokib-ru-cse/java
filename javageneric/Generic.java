12b32ddf433c4c6febee9752b2197d3e0b5e90c7
rokib_ru_cse <rokib.ahmed@bizzntek.com>
2023-07-26 01:58:17 +0600

    .

--- javageneric/Generic.java
+++ javageneric/Generic.java
@@ -6,6 +6,24 @@ import java.util.List;
 public class Generic {
 
     public static void main(String[] args) {
-        System.out.println("ssssssssssss");
+        Generic t = new Generic();
+
+        PairTest<Integer> pairTest1 = t.new PairTest<Integer>();
+        pairTest1.first = 1;
+        pairTest1.second = 2;
+        System.out.println(pairTest1.first);
+        System.out.println(pairTest1.second);
+
+        PairTest<String> pairTest2 = t.new PairTest<>();
+        pairTest2.first = "abc";
+        pairTest2.second = "xyz";
+        System.out.println(pairTest2.first);
+        System.out.println(pairTest2.second);
     }
+
+    class PairTest<T> {
+        T first;   // T = int,string, boolean, double, any type
+        T second;
+    }
+
 }
--- javageneric/RokibComparator.java
+++ javageneric/RokibComparator.java
@@ -5,10 +5,10 @@ import java.util.Comparator;
 public class RokibComparator {
 
 
-    public static Comparator<Pair> PairComparator = new Comparator<Pair>() {
-        @Override
-        public int compare(Pair o1, Pair o2) {
-            return 0;
-        }
-    };
+//    public static Comparator<Pair> PairComparator = new Comparator<Pair>() {
+//        @Override
+//        public int compare(Pair o1, Pair o2) {
+//            return 0;
+//        }
+//    };
 }
