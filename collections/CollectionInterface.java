877faebca6653ec5c9d7c8c91fd9b3a193274913
rokib_ru_cse <rokib.ahmed@bizzntek.com>
2023-09-19 23:31:10 +0600

    .

---
+++ collections/CollectionInterface.java
@@ -0,0 +1,24 @@
+package collections;
+
+import java.util.*;
+
+public class CollectionInterface {
+
+
+    public static void main(String[] args) {
+        Collection<Integer> integers = new ArrayList<>();
+
+        Collections collections = null;
+        Collections.swap((List<?>) integers, 1, 2);
+
+
+        //        list
+
+        List<Integer> integerArrayList = new ArrayList<>();
+        LinkedList<Integer> integerLinkedList = new LinkedList<>();
+        Vector<Integer> integerVector = new Vector<>();
+        Stack<Integer> integerStack = new Stack<>();
+        //!        list
+
+    }
+}
