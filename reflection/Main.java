01f3ffd1965a5514410fcb0e31b641f0bdeb0bee
rokibahmed.th.bd <rokib.ahmed@therapservies.net>
2023-10-08 19:00:36 +0600

    java Reflection

---
+++ reflection/Main.java
@@ -0,0 +1,18 @@
+package reflection;
+
+import java.lang.reflect.Field;
+
+public class Main {
+    private int data = 10;
+}
+
+class Reflection {
+    public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException {
+        Main main = new Main();
+        Field field = main.getClass().getDeclaredField("data");
+        field.setAccessible(true);
+        System.out.println("Previous value : " + field.get(main));
+        field.set(main,20);
+        System.out.println("Current value : " + field.get(main));
+    }
+}
